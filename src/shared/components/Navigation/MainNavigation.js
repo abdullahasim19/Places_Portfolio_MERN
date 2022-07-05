0bc9e131c2d6efd66112687f0c361a42e00a2032
abdullahasim19 <abdullahasimlhe@gmail.com>
2022-07-05 19:41:43 +0500

    Responsiveness Done

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+
 import './MainNavigation.css';
 
 export default function MainNavigation(props) {
+  const [drawerIsOpen,setDrawerIsOpen]=useState(false)
+  const openDrawer=()=>{
+    setDrawerIsOpen(true)
+  }
+  const closeDrawer=()=>{
+    setDrawerIsOpen(false)
+  }
+
   return (
+    <React.Fragment>
+      {drawerIsOpen&&<Backdrop onClick={closeDrawer}/>}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+          <nav className='main-navigation__drawer-nav'>
+            <NavLinks/>
+          </nav>
+        </SideDrawer> 
+   
     <MainHeader>
-        <button className='main-navigation__menu-btn'>
+        <button className='main-navigation__menu-btn' onClick={openDrawer}>
             <span/>
             <span/>
             <span/>
@@ -14,9 +34,10 @@ export default function MainNavigation(props) {
         <h1 className='main-navigation__title'>
             <Link to='/'>Your Places</Link>
         </h1>
-        <nav>
-            ...
+        <nav className='main-navigation__header-nav'>
+           <NavLinks/>
         </nav>
     </MainHeader>
+    </React.Fragment>
   )
 }
